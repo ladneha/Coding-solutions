97f936229f8b58502f477cf0af72d106f6bd025b
Neha Lad <32019682+ladneha@users.noreply.github.com>
2018-07-18 21:49:41 -0400

    addition to previous

--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -7,3 +7,16 @@ class SearchInRotatedSortedArray {
         return hs.contains(target);
     }
 }
+// when you need to return index and array has no duplicates
+class Solution {
+    public int search(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap();
+        for(int i=0; i<nums.length; i++){
+            hm.put(nums[i], i);
+        }
+        if(hm.containsKey(target)){
+            return hm.get(target);
+        }
+        return -1;
+    }
+}
